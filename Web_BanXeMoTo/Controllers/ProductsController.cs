754548412644d3078efdb2beb3234bb61c8472d8
LongTre <hoanglong29032000@gmail.com>
2021-04-18 22:19:21 +0700

    finish load data to layout

---
+++ Web_BanXeMoTo/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web_BanXeMoTo.Models;
+
+namespace Web_BanXeMoTo.Controllers
+{
+    public class ProductsController : Controller
+    {
+        QLMoToContext database = new QLMoToContext();
+        public IActionResult Products()
+        {
+            return View(database.MauXes.ToList());
+        }
+
+        public IActionResult ProductsDetail(string id)
+        {
+            return View(database.MauXes.Where(model=>model.Idmau==id).FirstOrDefault());
+        }
+    }
+}
